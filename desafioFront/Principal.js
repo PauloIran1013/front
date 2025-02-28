e00e67d950926035aa031745c6da70e462f0361b
PauloIran1013 <julioirancoelho@gmail.com>
2025-02-28 13:41:03 -0300

    fiz carrosel

--- desafioFront/Principal.js
+++ desafioFront/Principal.js
@@ -8,3 +8,46 @@ if(menu.style.display === "block"){
     menu.style.display ="block"
 }
 })
+
+let index = 0 
+
+function mudarImagem(direcao) {
+    const items = document.querySelectorAll(".item");
+
+    index += direcao;
+
+    if (index < 0) {
+        index = items.length - 1;
+    }else if (index >= items.length) {
+        index = 0  ;
+    }
+    
+    const banner = document.querySelector(".banner");
+    banner.style.transform = `translateX(${-index * 100}%) `;
+}
+
+
+function iniciarCarrossel() {
+    setInterval(()=>{
+        mudarImagem(1)
+    },3000)
+    
+}
+
+window.onload = iniciarCarrossel;
+
+function mudarImagem2(direcao) {
+    const items = document.querySelectorAll(".itemCard");
+
+    index += direcao;
+
+    if (index < 0) {
+        index = items.length - 1;
+    }else if (index >= items.length) {
+        index = 0  ;
+    }
+    
+    const banner = document.querySelector(".CardsTrocar");
+    banner.style.transform = `translateX(${-index * 100}%) `;
+}
+
